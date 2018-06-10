1a28aedf50c6e36c3b82a90709bf0b5c89791637
Abanoub-Emil <Champion@Bakhoums-MacBook-Pro.local>
2018-06-10 21:38:16 +0200

    Initial Commit

---
+++ FaceBookApi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceBookApi
+//
+//  Created by Champion on 5/19/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceBookApi/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  FaceBookApi
+//
+//  Created by Champion on 5/19/18.
+//  Copyright © 2018 ITI. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    // Optional: Place the button in the center of your view.
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+    
+    if ([FBSDKAccessToken currentAccessToken]) {
+        NSLog(@"user is logged in");
+    }
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
